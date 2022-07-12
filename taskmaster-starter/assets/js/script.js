5337b35cba1c9a4154eb55060bb4da6975da3b5b
Brenda  Sosa <brenda.sosa@gmail.com>
2022-07-12 09:42:21 -0400

    added changes using jquery

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -89,9 +89,15 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+  $(".list-group").on("click", "p", function () {
+    var text = $(this).text().trim();
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+  });
 });
 
 // load tasks for the first time
 loadTasks();
+
 
 
